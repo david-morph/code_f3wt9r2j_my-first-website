c17411363d47d011a6df784d6d195ec52bffa0e4
David Anderson <david@morph.so>
2024-10-04 03:10:32 +0000

    Experimenting

--- app/page.js
+++ app/page.js
@@ -1,14 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = Home;
-const image_1 = __importDefault(require("next/image"));
 function Home() {
     return (<div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold mb-8">Welcome to my Landing Page!</h1>
-      <image_1.default src="/cat.jpg" alt="A cute cat" width={500} height={500}/>
+
     </div>);
 }
 //# sourceMappingURL=page.js.map
--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold mb-8">Welcome to my Landing Page!</h1>
-      <Image 
-        src="/cat.jpg"
-        alt="A cute cat"
-        width={500}
-        height={500}
-      />
+
     </div>
   )
 }
